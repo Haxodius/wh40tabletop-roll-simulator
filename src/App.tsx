0458fbe14938a7b5f5a07945d0ad45809d2b18c7
Haxodius <murofurki2002@hotmail.com>
2024-10-05 21:04:06 +0300

    initial files for project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Project is under development :)
+    </div>
+  );
+}
+
+export default App;
